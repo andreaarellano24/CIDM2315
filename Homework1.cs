9757aff92f72372ee6a5e659a6cacae54d79eff6
andreaarellano24 <andreaarella.a@gmail.com>
2026-01-30 20:36:13 -0600

    Update Homework1.cs

--- Homework1.cs
+++ Homework1.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Homework1");
+        double x = 2.5;
+        double y = 3.3;
+        double z = 4 * x * x + 3 * y;
+
+        Console.WriteLine($"x = {x}, y = {y}");
+        Console.WriteLine($"The Value of z is {z}");
     }
 }
